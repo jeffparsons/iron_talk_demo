b886a7a157d8c64cd19011269e70ca3bd21b26bc
Jeff Parsons <jeff.parsons@stileeducation.com>
2016-09-20 15:39:46 +1000

    Chapter 3: Hello, API!
    What we have so far with `staticfile` is a handler.
    It takes a request and produces a response. All `staticfile`
    knows how to do is try to match request paths to paths on disk.
    
    If we want to serve up static files _and_ API requests, we're going
    to do a couple more clever things:
    
    - route request starting with '/api' to their specific handlers
    - try to serve up everything else as a static file
    
    Thankfully, Iron has pieces for these tasks:
    
    - `router` to match on paths, path parameters, HTTP request verbs
    - `mount` to mount multiple handlers at different URL prefixes
    
    ( Hack it all together now... )
    
    What we're passing to this router has to implement Handler.
    Fortunately there's a blanket implementation for certain types
    of functions, so as long as we conform to that interface, we're all good.
    
    ( And then to demonstrate that it still works... )
    
        curl localhost:8888/api/submissions
        curl localhost:8888

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 extern crate iron;
 extern crate staticfile;
+extern crate mount;
+extern crate router;
 
 use iron::*;
 use staticfile::*;
-
+use mount::*;
+use router::*;
 
 fn main() {
     let static_handler = Static::new("web/");
 
+    let mut api_handler = Router::new();
+    api_handler.get("/submissions", index_submissions, "is");
+    api_handler.get("/comments", index_comments, "ic");
+    api_handler.post("/comments", create_comment, "cc");
+
+    let mut mount = Mount::new();
+    mount.mount("/api", api_handler);
+    mount.mount("/", static_handler);
+
     let addr = "0.0.0.0:8888";
     println!("Listening on {}...", addr);
     println!("Try http://{}!", addr);
-    Iron::new(static_handler).http(addr).unwrap();
+    Iron::new(mount).http(addr).unwrap();
+}
+
+fn index_submissions(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "[]")))
+}
+
+fn index_comments(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "[]")))
+}
+
+fn create_comment(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Created)))
 }
