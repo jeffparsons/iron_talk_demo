7ba11baf721c405b42a52720d852e269a5773242
Jeff Parsons <jeff.parsons@stileeducation.com>
2016-09-20 15:21:45 +1000

    Chapter 2: Hello, web! (Static content only)
    Let's try to serve up a static file. We're going to need
    a couple of things from Iron:
    
    - The core `iron` crate
    - A bit of middleware called `staticfile`
    
    Let's try it:
    
        curl localhost:8888
    
    404! That's a good start. It means we've actually got a server.
    But of course it's a 404; we didn't actually give it anything to serve.
    
    ( Put something in web now. )

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate iron;
+extern crate staticfile;
+
+use iron::*;
+use staticfile::*;
+
+
 fn main() {
-    println!("Hello, world!");
+    let static_handler = Static::new("web/");
+
+    let addr = "0.0.0.0:8888";
+    println!("Listening on {}...", addr);
+    println!("Try http://{}!", addr);
+    Iron::new(static_handler).http(addr).unwrap();
 }
